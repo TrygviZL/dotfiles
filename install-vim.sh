2c78d596afadcd178a26cea0f3c7340027145fc8
TrygviZL <trlk@lundbeck.com>
2023-01-10 13:52:13 +0100

    messed up file

--- install-vim.sh
+++ install-vim.sh
@@ -27,16 +27,6 @@ mkdir $HOME/.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# get .vimrc
-wget https://raw.githubusercontent.com/TrygviZL/my-dotfiles/main/.vimrc
-mv .vimrc $HOMEasdasd
-
-# get coc-settings
-wget https://raw.githubusercontent.com/TrygviZL/my-dotfiles/main/coc-settings.json
-mv coc-settings.json $HOME/.vim
-
-# install plugins 
-vim +PlugInstall
 
 # install fish-shell
 sudo apt-add-repository ppa:fish-shell/release-2
