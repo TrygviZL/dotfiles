ca4040129a608eb5db0ff9b2962023b346598c88
Trygvi Zachariassen Laksafoss <56014816+TrygviZL@users.noreply.github.com>
2023-03-14 14:36:27 +0100

    routing

--- install-vim.sh
+++ install-vim.sh
@@ -3,6 +3,14 @@
 set -o errexit
 set -o nounset
 
+# fix possible vpm routing issue
+sudo rm -rf /etc/resolv.conf
+sudo tee /etc/resolv.conf << EOF
+search yourbase.domain.local
+nameserver 8.8.8.8
+nameserver 1.1.1.1
+EOF
+
 sudo apt-get update
 
 # Uninstall old vim version
