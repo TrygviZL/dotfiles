c610c93955584f1d6405b02b9794ca448776186b
Trygvi Zachariassen Laksafoss <56014816+TrygviZL@users.noreply.github.com>
2023-08-01 08:15:12 +0200

    Update install-vim.sh

--- install-vim.sh
+++ install-vim.sh
@@ -1,5 +1,7 @@
+
 #!/usr/bin/env bash
 
+DEBIAN_FRONTEND=noninteractive
 set -o errexit
 set -o nounset
 
@@ -24,7 +26,6 @@ sudo apt-add-repository ppa:jonathonf/vim
 sudo apt update
 sudo apt install vim
 
-
 # install nodejs
 sudo apt install nodejs
 
@@ -46,15 +47,6 @@ mv coc-settings.json $HOME/.vim
 # install plugins 
 vim +PlugInstall
 
-# install fish-shell
-sudo apt-add-repository ppa:fish-shell/release-2
-sudo apt-get update
-sudo apt-get install fish
-
-# install oh-my-fish and set theme
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-omf install lavender
-
 # install aws cli
 sudo apt install unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
@@ -65,4 +57,11 @@ sudo ./aws/install
 sudo apt install python3-pip
 pip install dbt-core dbt-snowflake
 
+# Install saml2aws
+mkdir -p ~/.local/bin
+CURRENT_VERSION=$(curl -Ls https://api.github.com/repos/Versent/saml2aws/releases/latest | grep 'tag_name' | cut -d'v' -f2 | cut -d'"' -f1)
+wget -c "https://github.com/Versent/saml2aws/releases/download/v${CURRENT_VERSION}/saml2aws_${CURRENT_VERSION}_linux_amd64.tar.gz" -O - | tar -xzv -C ~/.local/bin
+chmod u+x ~/.local/bin/saml2aws
 
+# adds convenient s2a command for logging into saml2aws
+printf '\nfunction s2a {\n\tsaml2aws login --force\n\teval \x27$( $(which saml2aws) script --shell=bash --profile=$@)\x27\n}' >> ~/.bashrc; source ~/.bashrc
